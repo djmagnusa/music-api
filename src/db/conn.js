c0b2587bb2fe92f229fde3982bc360b9a46f62f5
djmagnusa <pratush.bh@gmail.com>
2021-09-05 02:49:40 +0530

    Initial commit

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/music", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection established")
+}).catch((e) => {
+    console.log("Check your connection")
+})
