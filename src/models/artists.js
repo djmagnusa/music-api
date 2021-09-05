21c7891b6de73f27d0e7780a2977084b823306ec
djmagnusa <pratush.bh@gmail.com>
2021-09-05 20:07:04 +0530

    Add artist schema

--- src/models/artists.js
+++ src/models/artists.js
@@ -1,5 +1,42 @@
+const express = require("express");
 const mongoose = require("mongoose");
 
 const artistSchema = new mongoose.Schema({
+    artist: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    members: {
+        type: [String],
+        trim: true
+    },
+
+    genre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    songs: {
+        type: [String],
+        required: true,
+        trim: true
+    },
+
+    lyrics: {
+        type: [String],
+        required: true,
+        trim: true 
+    }
+
+
+
+
+
+
+
 
 })
