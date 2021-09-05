d0770dd54e850c9b36fcbe6be10d271b21e0a6c0
djmagnusa <pratush.bh@gmail.com>
2021-09-05 20:05:04 +0530

    Fix mongoose connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/music-api", {
-    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
