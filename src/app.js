0ccb3488fc5852d017436a57b3e98463ad817bae
djmagnusa <pratush.bh@gmail.com>
2021-09-06 22:13:17 +0530

    Add post request

--- src/app.js
+++ src/app.js
@@ -6,13 +6,16 @@ const Artists = require("../src/models/artists")
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.use(express.json());
+app.use(express.json()); //we can json data from postman
 
 app.post("/artists", async(req, res) => {
     try{
-
+        const addingArtists = new Artists(req.body)
+        console.log(req.body)
+        const insertArtists = await addingArtists.save();
+        res.send(insertArtists)
     } catch(e) {
-        
+        res.send(e);
     }
 })
 
