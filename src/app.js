2cc8d88bee2c77e59939cb53cc0bc0e0b96b8b4d
djmagnusa <pratush.bh@gmail.com>
2021-09-08 21:49:08 +0530

    Import router

--- src/app.js
+++ src/app.js
@@ -1,66 +1,16 @@
 const express = require('express');
+
 require("./db/conn");
 
-const Artists = require("../src/models/artists")
+//const Artists = require("../src/models/artists")
+
+const router = require("./routers/artist");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.use(express.json()); //we can json data from postman
-
-app.post("/artists", async(req, res) => {
-    try{
-        const addingArtists = new Artists(req.body)
-        console.log(req.body)
-        const insertArtists = await addingArtists.save();
-        res.status(201).send(insertArtists)
-    } catch(e) {
-        res.status(400).send(e);
-    }
-})
-
-app.get("/artists", async (req, res) => {
-    try{
-        const getArtists = await Artists.find({}).sort({"name": 1});
-        res.status(201).send(getArtists);
-    } catch(e) {
-        res.status(400).send(e);
-    }
-})
-
-app.get("/artists/:id", async (req, res) => {
-    try{
-        const _id = req.params.id;
-        const getArtist = await Artists.findById({_id:_id});
-        res.send(getArtist);
-    } catch(e) {
-        res.status(400).send(e);
-    }
-})
-
-app.patch("/artists/:id", async (req, res) => {
-    try{
-        const _id = req.params.id;
-        const getArtist = await Artists.findByIdAndUpdate({_id:_id}, req.body, {
-            new: true //to see the updated data
-        });
-        res.send(getArtist);
-
-    } catch(e) {
-        res.status(400).send(e);
-    }
-})
-
-app.delete("/artists/:id", async (req, res) => {
-    try{
-    
-        const getArtist = await Artists.findByIdAndDelete(req.params.id);
-        res.send(getArtist);
-    }
-    catch(e) {
-        res.status(500).send(e);
-    }
-})
+app.use(express.json()); //we can json data from postman (middleware)
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
